0751d6eebfb094249b7397f3ff2a6f09adb92864
James Frasche <soapboxcicero@gmail.com>
2010-07-12 10:28:48 -0700

    Removed cmds/util.go:possiblyInvokable now that the package is modernized

--- src/gelo/commands/util.go
+++ src/gelo/commands/util.go
@@ -5,13 +5,6 @@ import (
 	"math"
 )
 
-func possiblyInvokable(w gelo.Word) bool {
-	_, ok1 := w.(gelo.Symbol) //can't know it will always point to an invokable
-	_, ok2 := w.(gelo.Quote)
-	_, ok3 := w.(gelo.Alien)
-	return ok1 || ok2 || ok3
-}
-
 func IndexError(vm *gelo.VM, idx int, w gelo.Word) {
 	gelo.RuntimeError(vm, "Invalid index", idx, "in", w)
 }
